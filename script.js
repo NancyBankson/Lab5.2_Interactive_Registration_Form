d7d6c74e27fd010c3c5cb004af3093f5040671b1
Nancy Bankson <nbankson3@gmail.com>
2025-11-07 11:00:10 -0600

    Event listeners updated

--- script.js
+++ script.js
@@ -24,7 +24,48 @@ username.addEventListener("input", function(event) {
     usernameError.textContent = username.validationMessage;
 })
 
+email.addEventListener("input", function(event) {
+    if (email.validity.patternMismatch) {
+        email.setCustomValidity("Email address must use the format example@.domain.com");
+    } else if (email.validity.valueMissing) {
+      console.log("invalid entry");
+        email.setCustomValidity("Please enter your email address");
+    }    
+    else {email.setCustomValidity("")}
+    emailError.textContent = email.validationMessage;
+})
+
+password.addEventListener("input", function(event) {
+    if (password.validity.valueMissing) {
+        password.setCustomValidity("Please enter a password");
+    } else if (password.validity.tooShort) {
+        password.setCustomValidity("Password must be at least 8 characters")
+    } else if (password.validity.patternMismatch) {
+        password.setCustomValidity("Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, and a number.")
+    } else {password.setCustomValidity("")}
+    passwordError.textContent = password.validationMessage;
+})
+
+confirmPassword.addEventListener("input", function(event) {
+    if (confirmPassword.validity.valueMissing) {
+        confirmPassword.setCustomValidity("Please re-enter your password");
+    } else if (password.value != confirmPassword.ariaValueMax) {
+        confirmPassword.setCustomValidity("Passwords must match")
+    } else {confirmPassword.setCustomValidity("")}
+    confirmPasswordError.textContent = username.validationMessage;
+})
+
 
 submit.addEventListener("click", function() {
     localStorage.setItem("username", username.value);
+    if (email.validity.patternMismatch) {
+        email.setCustomValidity("Email address must use the format example@.domain.com");
+        email.focus();
+    } else if (email.validity.valueMissing) {
+      console.log("invalid entry");
+        email.setCustomValidity("Please enter your email address");
+        email.focus();
+    }    
+    else {email.setCustomValidity("")}
+    emailError.textContent = email.validationMessage;
 })
