322c09ddc4636edecb81313a580bd038d9c5eeef
Nancy Bankson <nbankson3@gmail.com>
2025-11-07 12:56:30 -0600

    Submit button completed

--- script.js
+++ script.js
@@ -6,66 +6,99 @@ const usernameError = document.getElementById("usernameError");
 const emailError = document.getElementById("emailError");
 const passwordError = document.getElementById("passwordError");
 const confirmPasswordError = document.getElementById("confirmPasswordError");
-const submit = document.getElementById("submitButton");
+const submitButton = document.getElementById("registrationForm");
+let usernameCorrect = true;
+let emailCorrect = false;
+let passwordCorrect = false;
+let confirmPasswordCorrect = false;
 
 window.addEventListener('load', () => {
-  const user = localStorage.getItem("username");
-  if (user) {
-    username.value = user;
-  }
+    const user = localStorage.getItem("username");
+    if (user) {
+        username.value = user;
+    }
 });
 
-username.addEventListener("input", function(event) {
+username.addEventListener("input", function (event) {
     if (username.validity.valueMissing) {
         username.setCustomValidity("Please enter a Username (I'm not yelling)");
+        usernameCorrect = false;
     } else if (username.validity.tooShort) {
-        username.setCustomValidity("Username must be at least 3 characters")
-    } else {username.setCustomValidity("")}
+        username.setCustomValidity("Username must be at least 3 characters");
+        usernameCorrect = false;
+    } else {
+        username.setCustomValidity("");
+        usernameCorrect = true;
+    }
     usernameError.textContent = username.validationMessage;
 })
 
-email.addEventListener("input", function(event) {
+email.addEventListener("input", function (event) {
     if (email.validity.patternMismatch) {
         email.setCustomValidity("Email address must use the format example@.domain.com");
     } else if (email.validity.valueMissing) {
-      console.log("invalid entry");
+        console.log("invalid entry");
         email.setCustomValidity("Please enter your email address");
-    }    
-    else {email.setCustomValidity("")}
+    }
+    else {
+        email.setCustomValidity("");
+        emailCorrect = true;
+    }
     emailError.textContent = email.validationMessage;
 })
 
-password.addEventListener("input", function(event) {
+password.addEventListener("input", function (event) {
     if (password.validity.valueMissing) {
         password.setCustomValidity("Please enter a password");
     } else if (password.validity.tooShort) {
         password.setCustomValidity("Password must be at least 8 characters")
     } else if (password.validity.patternMismatch) {
         password.setCustomValidity("Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, and a number.")
-    } else {password.setCustomValidity("")}
+    } else {
+        password.setCustomValidity("");
+        passwordCorrect = true;
+    }
     passwordError.textContent = password.validationMessage;
 })
 
-confirmPassword.addEventListener("input", function(event) {
+confirmPassword.addEventListener("input", function (event) {
     if (confirmPassword.validity.valueMissing) {
         confirmPassword.setCustomValidity("Please re-enter your password");
     } else if (password.value != confirmPassword.value) {
         confirmPassword.setCustomValidity("Passwords must match")
-    } else {confirmPassword.setCustomValidity("")}
+    } else {
+        confirmPassword.setCustomValidity("");
+        confirmPasswordCorrect = true;
+    }
     confirmPasswordError.textContent = confirmPassword.validationMessage;
 })
 
 
-submit.addEventListener("click", function() {
-    localStorage.setItem("username", username.value);
-    if (email.validity.patternMismatch) {
-        email.setCustomValidity("Email address must use the format example@.domain.com");
+submitButton.addEventListener("submit", function(event) {    
+    event.preventDefault();
+    // localStorage.setItem("username", username.value);
+    console.log(usernameCorrect);
+    if (usernameCorrect === false) {
+        alert("Enter a Username, must be at least 3 characters");
+        username.focus();
+    } else { localStorage.setItem("username", username.value); }
+    if (emailCorrect === false) {
+        alert("Please enter email address in format example@domain.com");
         email.focus();
-    } else if (email.validity.valueMissing) {
-      console.log("invalid entry");
-        email.setCustomValidity("Please enter your email address");
-        email.focus();
-    }    
-    else {email.setCustomValidity("")}
-    emailError.textContent = email.validationMessage;
+    }
+    if (passwordCorrect === false) {
+        alert("Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, and a number");
+        password.focus();
+    }
+    if (confirmPasswordCorrect === false) {
+        alert("Passwords must match");
+        confirmPassword.focus();
+    }
+    if ((usernameCorrect === true) && (emailCorrect === true) && (passwordCorrect === true) && (confirmPasswordCorrect === true)) {
+        alert("Qapla!  That's Klingon for 'Success!'");
+        username.value = "";
+        email.value = "";
+        password.value = "";
+        confirmPassword.value = "";
+    }
 })
